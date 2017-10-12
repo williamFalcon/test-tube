867f3f794089b39683d5da891866b7d173edc242
William Falcon <waf2107@columbia.edu>
2017-10-12 08:41:46 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-version = '0.33'
+version = '0.34'
 
 setup(name='test_tube',
       packages=find_packages(),
       version=version,
       description='Experiment logger and visualizer',
       author='William Falcon',
-      install_requires=['scikit-image==0.12.3'],
+      install_requires=['scikit-image>=0.12.3'],
       author_email='will@hacstudios.com',
       url='https://github.com/williamFalcon/test_tube',
       download_url='https://github.com/williamFalcon/test_tube/archive/{}.tar.gz'.format(version),
