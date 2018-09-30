a275406b91325cc50ccf17e8e07a0f18b139bbf1
William Falcon <waf2107@columbia.edu>
2018-09-30 19:06:44 -0400

    trying test

--- tests/hpc_test.py
+++ tests/hpc_test.py
@@ -1,9 +1,12 @@
 import pytest
 from test_tube.hpc import SlurmCluster
+from test_tube.argparse_hopt import HyperOptArgumentParser
 
 
 def test_slurm_time_to_seconds():
-    cluster = SlurmCluster('/home/travis', None)
+    parser = HyperOptArgumentParser()
+    parsed = parser.parse_args()
+    cluster = SlurmCluster('/home/travis', parsed)
 
     assert cluster.slurm_time_to_seconds('15:00') == 900
     assert cluster.slurm_time_to_seconds('1-12:20:12') == 130812
