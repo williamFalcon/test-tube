b4e88f5cf445634057449bd6c59694010d0bb533
William Falcon <falc@fb.com>
2019-07-03 16:26:48 -0400

    added init with ddp class method

--- test_tube/log.py
+++ test_tube/log.py
@@ -49,6 +49,20 @@ class DDPExperiment(object):
 
 
 class Experiment(SummaryWriter):
+
+
+    @classmethod
+    def init_with_ddp_exp(cls, exp):
+        return Experiment(
+            name=exp.name,
+            debug=exp.debug,
+            version=exp.version,
+            save_dir=exp.save_dir,
+            autosave=exp.autosave,
+            description=exp.description,
+            create_git_tag=exp.create_git_tag
+        )
+
     def __init__(
         self,
         name='default',
